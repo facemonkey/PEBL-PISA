0fbaaa342a4770ef59163024f18186b9069770f7
facemonkey <willwieland@gmail.com>
2016-10-20 16:28:16 -0400

    First upload

---
+++ tet-cpp-client-master/CMakeFiles/GazeApiLib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GazeApiLib.dir/src/gazeapi.cpp.o"
+  "CMakeFiles/GazeApiLib.dir/src/gazeapi_parser.cpp.o"
+  "CMakeFiles/GazeApiLib.dir/src/gazeapi_socket.cpp.o"
+  "libGazeApiLib.pdb"
+  "libGazeApiLib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GazeApiLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tet-cpp-client-master/Gazelib Library/Gazelib Library/Gazelib Library.h
@@ -0,0 +1,19 @@
+//
+//  Gazelib Library.h
+//  Gazelib Library
+//
+//  Created by Shane on 6/13/16.
+//  Copyright © 2016 PEBL Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Gazelib Library.
+FOUNDATION_EXPORT double Gazelib_LibraryVersionNumber;
+
+//! Project version string for Gazelib Library.
+FOUNDATION_EXPORT const unsigned char Gazelib_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gazelib_Library/PublicHeader.h>
+
+
